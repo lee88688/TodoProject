55b4d2a68b05b3a5fcab7e71735e8143efb22fc5
lisi <lisi@cmiot.chinamobile.com>
2019-07-02 14:49:10 +0800

    add vuetify

---
+++ src/views/todolist/FolderList.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-layout column style="height: 100%;">
+    <v-flex shrink class="folder-list-menu-search py-1">
+      <v-icon class="folder-list-menu-icon">mdi-menu</v-icon>
+      <v-text-field solo clearable hide-details flat append-icon="mdi-folder-search"></v-text-field>
+    </v-flex>
+    <v-flex grow>
+      <v-layout column style="height: 100%;">
+        <v-flex shrink class="folder-list-user-info">
+          <v-menu>
+            <template #activator="{ on }">
+              <v-layout row align-center>
+                <v-avatar>
+                  <v-icon>mdi-vuejs</v-icon>
+                </v-avatar>
+                <span>lisi</span>
+              </v-layout>
+            </template>
+          </v-menu>
+        </v-flex>
+        <v-flex grow>folder</v-flex>
+        <v-flex shrink>add new folder</v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'FolderList'
+}
+</script>
+
+<style lang="scss" scoped>
+.folder-list-menu-search {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.folder-list-menu-icon {
+  padding: 10px 10px;
+}
+
+.folder-list-user-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/views/todolist/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container full-height fluid class="pa-0">
+    <v-layout row fill-height>
+      <v-flex class="left-folder-list">
+        <folder-list></folder-list>
+      </v-flex>
+      <v-flex>center</v-flex>
+      <v-flex class="right-todo-detail">right</v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import FolderList from '@/views/todolist/FolderList'
+export default {
+  components: {
+    FolderList
+  }
+}
+</script>
+
+<style scoped>
+.left-folder-list, .right-todo-detail {
+  width: 300px;
+  flex: 0 0 auto;
+}
+</style>
