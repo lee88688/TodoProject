8040f5d4786b4db2efaeebfdc1fb02722a70baa5
lisi <lee2py@gmail.com>
2019-07-16 17:51:02 +0800

    use enter to add todo

--- src/views/todo/Index.vue
+++ src/views/todo/Index.vue
@@ -2,7 +2,6 @@
   <v-layout column fill-height>
     <v-flex shrink style="width: 100%;">
       <v-toolbar flat dense dark color="blue-grey lighten-1">
-      <!--<v-toolbar flat dense dark color="primary">-->
         <v-toolbar-title>{{ currentFolderName }}</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
@@ -13,10 +12,10 @@
     <v-flex grow class="pa-3" style="background-color: #78909C; overflow: hidden;">
       <v-layout column style="height: 100%;">
         <v-flex shrink>
-          <v-text-field solo prepend-inner-icon="mdi-plus" append-icon="mdi-send" placeholder="添加新任务" v-model="todoName" @click:append="addNewTodo"></v-text-field>
+          <v-text-field @click:append="addNewTodo" @keyup.enter="addNewTodo" v-model="todoName" solo prepend-inner-icon="mdi-plus" append-icon="mdi-send" placeholder="添加新任务"></v-text-field>
         </v-flex>
         <v-flex grow style="overflow: hidden; height: 0; position: relative;" class="ps" ref="scrollContainer">
-          <v-list dense class="transparent">
+          <v-list class="transparent">
             <template v-for="item in todos">
               <v-btn v-if="item.name" flat small dark class="min-width-0" :key="item.name">{{ item.name }}</v-btn>
               <v-list-tile ripple @click="clickTodo(t.id)" class="elevation-1 my-1" style="background-color: #fafafa; user-select: none;" v-for="t in item.todos" :key="t.id">
